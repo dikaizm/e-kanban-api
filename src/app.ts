16a1fd944b95d1eac63bc8dacf73c54c32567c6c
Dika <dikamaah@gmail.com>
2024-08-02 07:43:07 +0700

    feat: add cors origin

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,20 @@ dotenv.config();
 
 const app = express();
 
+const allowlist = ['https://ekanban-manufacture.vercel.app', 'http://localhost:5163'];
+const corsOptionsDelegate = function (req: any, callback: any) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptionsDelegate));
 app.use(express.json());
 
 // Routes
